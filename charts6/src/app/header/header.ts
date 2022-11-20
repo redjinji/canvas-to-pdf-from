3a6df122ac5073e6d4cdb87f82fd4c718467b9bb
tzafrir.raveh <redjinji@gmail.com>
2022-11-20 21:48:24 +0200

    add new fields and move other to the details page

--- charts6/src/app/header/header.ts
+++ charts6/src/app/header/header.ts
@@ -3,40 +3,41 @@ import {FormNavigationService} from "../form/form-navigation.service";
 import {UserAnthentityService} from "../login";
 
 @Component({
-    selector: 'Header-component',
-    templateUrl: './header.html'
+  selector: 'Header-component',
+  templateUrl: './header.html'
 })
 export class Header {
-    start:boolean=true;
-    camera:boolean=false;
-    angel:boolean=false;
-    details:boolean=false;
-    constructor(private formNavigationService: FormNavigationService, public auth:UserAnthentityService) {
-        formNavigationService.navigate.subscribe(this.activeIcon.bind(this));
-    }
-    
-    activeIcon(param) {
-        this.start=false;
-        this.camera=false;
-        this.angel=false;
-        this.details=false;
+  start: boolean = true;
+  camera: boolean = false;
+  angel: boolean = false;
+  details: boolean = false;
 
-        switch (param.position) {
-            case 8:
-                this.camera=true;
-                break;
-            case 9:
-                this.angel=true;
-                break;
-            case 10:
-                this.details=true;
-                break;
-            default:
-                this.start=true;
-        }
+  constructor(private formNavigationService: FormNavigationService, public auth: UserAnthentityService) {
+    formNavigationService.navigate.subscribe(this.activeIcon.bind(this));
+  }
+
+  activeIcon(param) {
+    this.start = false;
+    this.camera = false;
+    this.angel = false;
+    this.details = false;
+
+    switch (param.position) {
+      case 6:
+        this.camera = true;
+        break;
+      case 7:
+        this.angel = true;
+        break;
+      case 8:
+        this.details = true;
+        break;
+      default:
+        this.start = true;
     }
-    
-    goto(position){
-        this.formNavigationService.goto(position);
-    }
+  }
+
+  goto(position) {
+    this.formNavigationService.goto(position);
+  }
 }
